1dbe2647abb92c1e1e0b1cfca5bb2068fb84c261
azzazyy <azzazy.ashrafazzazy@gmail.com>
2026-05-11 13:46:33 +0200

    Completed day 1 it's super easy for anyone even if he/she doesn't have any experience in coding.

---
+++ Day001/Exercises.py
@@ -0,0 +1,13 @@
+print("HelloWorld!!") ###print function usede to print wahtever in its parenthesis and using the quotes to tell the computer that's not code and print this sentence as it is
+### PART ONE
+
+print("Hello", input("What is your name?")) ###we can also use the input function to get some input from the user and print it with the print function
+###PART TWO
+
+print(len(input("What is your name?")), "characters long") ###we can also use the len function to get the length of the input from the user and print it with the print function
+###PART THREE
+
+name = input("What is your name?") ###we can also store the input from the user in a variable and use it later
+length=len(name) ###we can also use the len function to get the length of the variable and store it in another variable
+print(name, "is", length, "characters long") ###we can also print the variables with the print function and use commas to separate them and it will print them with a space in between
+###PART FOUR
---
+++ Day001/project.py
@@ -0,0 +1,5 @@
+print("Welcome to brand name generator!")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+brand_name = city + " " + pet
+print("Your brand name could be " + brand_name)
